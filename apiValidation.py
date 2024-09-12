a63546dbe1c8aaf9a6c97ab2135054de5dd8b19e
NoMeCompila <cabafer1@gmail.com>
2024-09-12 03:23:55 -0300

    happy progremmers day

---
+++ apiValidation.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+endpoint: str = 'https://jsonplaceholder.typicode.com/posts'
+response = requests.get(endpoint)
+
+response_body = response.json()
+print(response_body)
+
+
